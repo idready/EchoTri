b68b0a3603aa5b7c58ffa58c1aff0caf24553699
idready <you_sombo@hotmail.fr>
2015-11-29 14:47:26 +0100

    Background on Homepage

--- app/scripts/controllers/ContactFormCtrl.js
+++ app/scripts/controllers/ContactFormCtrl.js
@@ -1,11 +1,18 @@
 
 'use strict';
 
-myApp.controller('ContactFormCtrl', ['$scope', function($scope) {
+myApp.controller('ContactFormCtrl', ['$scope', '$timeout', function($scope, $timeout) {
 
 
     $scope.echoForm = {};
+    $scope.form = {
 
+        handle: false,
+        submit: false
+    };
+
+    $scope.REGEX_EMAIL = '/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i';
+    // ^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$
     $scope.$on('loading', function(){
 
         console.log('loading');
@@ -18,7 +25,12 @@ myApp.controller('ContactFormCtrl', ['$scope', function($scope) {
             evt.preventDefault();
         }
 
+        $scope.handle = true;
         console.log($scope.echoForm);
+        $timeout(function(){
+            console.log('disabled')
+            $scope.handle = false;
+        }, 1000);
     }
 
 }]);
