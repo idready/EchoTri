f13cd9520382c5c2b3ae09385ab4dea8a4549ef3
idready <you_sombo@hotmail.fr>
2016-03-21 23:31:07 +0100

    Removed Blog link for now

--- app/wp/wp-content/themes/echotri/footer.php
+++ app/wp/wp-content/themes/echotri/footer.php
@@ -14,10 +14,10 @@
       <footer class="footer">
           <ul class="footer-links">
               <li class="footer-links__item">
-                <?php //Get this dynamically ?>
+                <?php //Get this id dynamically ?>
                 <a href="#" my-modal-link data-modal-event="modal-trigger" page-type="secondary" data-type="cd-modal-trigger" data-posticon="people-us" data-postId="84">Qui sommes nous ?</a>
               </li>
-              <li class="footer-links__item"><a href="#">Blog</a></li>
+              <?php /* <li class="footer-links__item"><a href="#">Blog</a></li> */ ?>
               <li class="footer-links__item"><a target="_blank" rel="noopener" href="https://www.facebook.com/EchoTri-545067608842775/">Facebook</a></li>
           </ul>
           <p class="footer-cpr">EchoTri tous droits reservés 2015</p>
