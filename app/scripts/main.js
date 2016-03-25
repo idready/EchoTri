69f004394a230d5185d0918705beebe6f24936a0
idready <you_sombo@hotmail.fr>
2016-03-25 20:54:12 +0100

    Section item animations

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,15 +7,44 @@ document.addEventListener('DOMContentLoaded', function(){
 
     //Hunt element to dispplay only if visible on viewport
     var sections = document.querySelectorAll('.info-section__item');
+    var count = 0;
+    var timer;
 
     for(var i = 0; i < sections.length; i++){
 
-        inViewport(sections[i], isVisible);
+        var el = sections[i];
+        var watcher = inViewport(el, isVisible);
+        var isInViewport = inViewport(el);
+        timer = setTimeout(watcher.watch, 1000);
+        inViewport(el, isVisible);
+
+        setTimeout(function(){
+
+            console.log(el, ' is in viewport : ',isInViewport);
+            if (isInViewport) {
+
+            } else {
+
+            }
+        }, 1500);
+
     }
 
     function isVisible(el) {
-
+        console.log('Watching: '+el);
         el.classList.add('visible');
     }
+
+    // function test(callback, time) {
+    //
+    //     console.log(callback);
+    //     console.log(time);
+    // }
+    // // setTimeout(function(){
+    // // //   watcher.dispose();
+    // //   console.log('Watcher', watcher);
+    // // //   clearTimeout(timer);
+    // //   console.info('myfancyDiv was visible '+count+' seconds in the last 10 seconds!');
+    // // }, 10000);
 
 });
